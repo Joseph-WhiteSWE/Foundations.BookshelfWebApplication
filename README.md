e8d267bb62d80bd93c4504b47039a243c7c48c4d
Joseph-WhiteSWE <white.joseph.jr@gmail.com>
2023-02-21 10:48:06 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Foundations.BookshelfWebApplication
+
+General Code
+(1) Design choices are explained and documented in comments.
+(2) DOM elements are styled via CSS.
+
+Navigation
+($) A Navigation Bar is included in the web app with links to other parts of the site e.g. "About", "Contact".
+(2) No real information is required. Dummy data or lorum ipsum is fine!
+
+Add Books
+(1) Users can enter book title, author, subjects, language and add new Books.
+(2) The Bookshelf instance is updated accordingly.
+(3) The Bookshelf instance renders Books accurately.
+
+Comments
+(1) Users can leave comments no longer than 280 characters on any Book they want.
+(2) Users can press a "Comment" button which reveals a `text` input element. Users can then type their comment and click "send" to add it to the Book.
+(3) The Book and Bookshelf instances are updated accordingly.
+(4) The comments persist even if a search or sort rerenders the Books.
+(5) Data can be stored locally or in state memory. A database is not required.
+
+TDD 5 Alternate: use the `filter` method on search terms so that the word "THE" doesn't affect any search term outcomes
---
+++ js/book-data.js
@@ -0,0 +1,56 @@
+const bookData = [
+    {
+      author: ["Burroughs, Edgar Rice"],
+      language: "en",
+      subject: [
+        "Adventure stories",
+        "PZ",
+        "Fantasy fiction",
+        "Tarzan (Fictitious character) -- Fiction",
+        "PS",
+        "Jungle animals -- Fiction",
+      ],
+      title: "The Beasts of Tarzan",
+    },
+    {
+      author: ["Kipling, Rudyard"],
+      language: "en",
+      subject: [
+        "India -- History -- 19th century -- Fiction",
+        "Feral children -- Fiction",
+        "Animals -- Fiction",
+        "Jungles -- Fiction",
+        "India -- Fiction",
+        "PR",
+        "Adventure stories, English",
+        "Mowgli (Fictitious character) -- Fiction",
+        "Short stories",
+        "Jungle animals -- Fiction",
+      ],
+      title: "The Jungle Book",
+    },
+    {
+      author: ["Sewell, Anna"],
+      language: "en",
+      subject: [
+        "Historical fiction",
+        "PZ",
+        "PR",
+        "Great Britain -- History -- 19th century -- Juvenile fiction",
+        "Human-animal relationships -- Juvenile fiction",
+        "Horses -- Juvenile fiction",
+      ],
+      title: "Black Beauty",
+    },
+    {
+      author: ["Lofting, Hugh"],
+      language: "en",
+      subject: [
+        "Fantasy literature",
+        "PZ",
+        "Human-animal communication -- Juvenile fiction",
+        "Animals -- Juvenile fiction",
+      ],
+      title: "The Story of Doctor Dolittle",
+    },
+]
