f7dafe1adeee28f130e663c5cc3672c0ef497eb4
Joseph-WhiteSWE <white.joseph.jr@gmail.com>
2023-02-22 13:08:24 -0600

    third commit

--- js/book.js
+++ js/book.js
@@ -10,10 +10,27 @@ class Book
 
     render() 
     {
-        let bookOfEli = document.createElement('li')
-        bookOfEli.innerText = this.title
+        let card = document.createElement('section')
+        card.classList.add('card')
 
-        return bookOfEli
+        let author = document.createElement('li')
+        author.innerText = this.author
+
+        let language = document.createElement('li')
+        language.innerText = this.language
+
+        let subject = document.createElement('li')
+        subject.innerText = this.subject
+
+        let title = document.createElement('li')
+        title.innerText = this.title
+
+        card.append(author)
+        card.append(language)
+        card.append(subject)
+        card.append(title)
+
+        return card
 
     }
 
--- js/bookshelf.js
+++ js/bookshelf.js
@@ -8,21 +8,29 @@ class Bookshelf {
     }
 
     //addBookToBookshelf(book){ 1hr mark on https://www.youtube.com/watch?v=k1eaLfoDY_E&ab_channel=FullstackAcademyPrograms
-        //example
+    //example
     //}
 
     render() {
-        let ulBible = document.createElement('ul')
-
-        for (const book of this.books) {
-            let bookOfEli = book.render()
-            ulBible.appendChild(bookOfEli)
-
-        }
+        let cardRender = this.books.map((book) => book.render())
         let bookSection = document.querySelector('#booklist')
         bookSection.innerHTML = '';
-        bookSection.appendChild(ulBible)
+
+        for (let card of cardRender) {
+            bookSection.append(card)
+        }
+
+        // let ulBible = document.createElement('ul')
+
+        // for (const book of this.books) {
+        //     let bookOfEli = book.render()
+        //     ulBible.appendChild(bookOfEli)
+
+        // }
+        // 
+        // bookSection.appendChild(ulBible)
 
 
+        return cardRender
     }
 }
